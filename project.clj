834e3d1bcb02cb390a2686225c6c680fcd949485
Duncan McGreggor <oubiwann@gmail.com>
2017-01-17 22:14:32 -0600

    Updated deps.

--- project.clj
+++ project.clj
@@ -5,19 +5,15 @@
              "-Dcookie=nocookie"
              "-Depmd_port=15000"]
   :description "Clojure + LFE"
-  :url "https://github.com/lfex/lfecljapp"
+  :url "https://github.com/clojang/lfecljapp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :resource-paths [ ]
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.12"]
-                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jmdk/jmxtools
-                                                    com.sun.jmx/jmxri]]
-                  [org.erlang.otp/jinterface "1.5.6"]]
+  :dependencies [[clojang "0.4.0-SNAPSHOT"]
+                 [clojusc/twig "0.3.0"
+                 [org.clojure/clojure "1.8.0"]]
   :main cljnode.core
   :source-paths ["src/clj"]
   :target-path "target/"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {
+    :uberjar {:aot :all}})
