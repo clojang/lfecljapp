e95721e342ffb7b5000dacbd9027f4864326704f
Duncan McGreggor <oubiwann@gmail.com>
2017-02-12 17:45:48 -0600

    New license for replacing code.

--- project.clj
+++ project.clj
@@ -1,17 +1,12 @@
 (defproject cljnode "0.4.0-SNAPSHOT"
-  :jvm-opts ["-Ddev_env=dev"
-             "-Dnode=cljnode@127.0.0.1"
-             "-Dmbox=mboxname"
-             "-Dcookie=nocookie"
-             "-Depmd_port=15000"]
-  :description "Clojure + LFE"
+  :description "Clojure Node App in an LFE/OTP System"
   :url "https://github.com/clojang/lfecljapp"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :resource-paths [ ]
+  :license {
+    :name "Apache License, Version 2.0"
+    :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[clojang "0.4.0-SNAPSHOT"]
                  [clojang/agent "0.4.0-SNAPSHOT"]
-                 [clojusc/twig "0.3.0"
+                 [clojusc/twig "0.3.0"]
                  [org.clojure/clojure "1.8.0"]]
   :main cljnode.core
   :source-paths ["src/clj"]
