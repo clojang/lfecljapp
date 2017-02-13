1e73a0d196b15b793ed7ca8d1fb81b1743485d5d
Duncan McGreggor <oubiwann@gmail.com>
2017-02-12 19:12:52 -0600

    Updated server.

--- src/clj/cljnode/core.clj
+++ src/clj/cljnode/core.clj
@@ -1,52 +1,57 @@
 (ns cljnode.core
-  (:require [cljnode.server :as server]
-            [clojang.core :as clojang :refer [! receive self]]
+  (:require [clojang.core :as clojang :refer [! receive self]]
             [clojang.node :as node]
             [clojang.mbox :as mbox]
             [clojure.core.match :refer [match]]
-            [clojure.tools.logging :as log])
+            [clojure.tools.logging :as log]
+            [clojusc.twig :as logger])
   (:gen-class))
 
 (defn server
-  ([]
-    (server
-      (System/getProperty "node.sname")
-      (System/getProperty "node.erlangcookie")))
-  ([node-name erlang-cookie]
-    (log/info
-      (format
-        "Started with params:\n\tnodename: %s\n\tcookie: %s"
-        node-name erlang-cookie))
-    (let [init-state 0]
-      (loop [png-count init-state]
-        (match (receive)
-          [:register caller]
-            (do
-              (mbox/link (self) caller)
-              (! caller :linked)
-              (recur png-count))
-          [:ping caller]
-            (do
-              (! caller :pong)
-              (recur (inc png-count)))
-          [:get-ping-count caller]
-            (do
-              (! caller png-count)
-              (recur png-count))
-          [:stop caller]
-            (do
-              (! caller :stopping)
-              :stopped)
-          [_ caller]
-            (do
-              (! caller [:error :unknown-command])
-              (recur png-count))
-          [_]
-            (do
-              (log/warn "Bad message received: improperly formatted")
-              (recur png-count)))))))
+  []
+  (logger/set-level! '[clojang cljnode] :info)
+  (log/info
+    (str
+      "Starting Clojure node with nodename = "
+      (System/getProperty "node.sname")))
+  (let [init-state 0]
+    (loop [png-count init-state]
+      (match (receive)
+        [:register caller]
+          (do
+            (log/info "Got :register request ...")
+            (mbox/link (self) caller)
+            (! caller :linked)
+            (recur png-count))
+        [:ping caller]
+          (do
+            (log/info "Got :ping request ...")
+            (! caller :pong)
+            (recur (inc png-count)))
+        [:get-ping-count caller]
+          (do
+            (log/info "Got :get-ping-count request ...")
+            (! caller png-count)
+            (recur png-count))
+        [:stop caller]
+          (do
+            (log/warn "Got :stop request ...")
+            (! caller :stopping)
+            :stopped)
+        [_ caller]
+          (do
+            (log/error "Bad message received: unknown command")
+            (! caller [:error :unknown-command])
+            (recur png-count))
+        [_]
+          (do
+            (log/error "Bad message received: improperly formatted")
+            (recur png-count))))))
+
+;; XXX implement API for communicating with running server
 
 (defn -main
     [& args]
-    (log/info "Starting Clojure node ...")
+    ;; XXX start server with core.async and create command channel
+    ;; XXX Create a command channel OTP message box for receiving messages
     (server))
